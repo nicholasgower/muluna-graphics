4ba66fef81e2949aa29cdb86fc7bc923c7b53ced
Nicholas Gower <nicholasgower01@gmail.com>
2025-04-20 18:41:56 -0700

    graphics: Fixed shadow locations of advanced steam turbine.

--- graphics/entities/advanced-steam-turbine/advanced-steam-turbine-east-shadow.lua
+++ graphics/entities/advanced-steam-turbine/advanced-steam-turbine-east-shadow.lua
@@ -2,6 +2,6 @@ return
 {
   width = 514,
   height = 225,
-  shift = util.by_pixel( 43, 39),
+  shift = util.by_pixel( 47-32, 39-32),
   line_length = 3,
 }
--- graphics/entities/advanced-steam-turbine/advanced-steam-turbine-north-shadow.lua
+++ graphics/entities/advanced-steam-turbine/advanced-steam-turbine-north-shadow.lua
@@ -2,6 +2,6 @@ return
 {
   width = 350,
   height = 425,
-  shift = util.by_pixel( 28.0, 41),
+  shift = util.by_pixel( 32.0-16, 41-32),
   line_length = 6,
 }
